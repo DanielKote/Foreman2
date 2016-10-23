3b5c14a607846b6e732320eeb47999243a1295d8
Brendan <Brendan@localhost>
2016-10-23 19:51:25 +0100

    Added Technology Class

---
+++ Foreman/Technology.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+
+namespace Foreman
+{
+    class Technology
+    {
+        public String Name { get; private set; }
+        public Bitmap Icon { get; set; }
+        public List<Recipe> unlocks { get; set; }
+        public Boolean Enabled { get; set; }
+
+        public String FriendlyName
+        {
+            get
+            {
+                if (DataCache.LocaleFiles["technology-name"].ContainsKey(Name))
+                {
+                    return DataCache.LocaleFiles["recipe-name"][Name];
+                }
+                else
+                {
+                    return Name;
+                }
+            }
+        }
+
+        public Technology(String Name)
+        {
+            this.Name = Name;
+            this.unlocks = new List<Recipe>();
+            this.Enabled = true;
+        }
+
+        public override int GetHashCode()
+        {
+            return this.Name.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (!(obj is Technology))
+            {
+                return false;
+            }
+
+            return (obj as Technology) == this;
+        }
+
+        public static bool operator ==(Technology technology1, Technology technology2)
+        {
+            if (object.ReferenceEquals(technology1, technology2))
+            {
+                return true;
+            }
+
+            if ((object)technology1 == null || (object)technology2 == null)
+            {
+                return false;
+            }
+
+            return technology1.Name == technology2.Name;
+        }
+
+        public static bool operator !=(Technology technology1, Technology technology2)
+        {
+            return !(technology1 == technology2);
+        }
+    }
+}
