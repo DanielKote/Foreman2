3112c39e59397be97b5d5f533773b90befba6858
DanielKote <danielkote@gmail.com>
2023-03-07 18:08:03 -0500

    UPDATE: switch to dev13

--- Foreman/Forms/MainForm.Designer.cs
+++ Foreman/Forms/MainForm.Designer.cs
@@ -551,7 +551,7 @@
 			this.VersionLabel.Name = "VersionLabel";
 			this.VersionLabel.Size = new System.Drawing.Size(115, 130);
 			this.VersionLabel.TabIndex = 18;
-			this.VersionLabel.Text = "Foreman v2.0 - dev.12";
+			this.VersionLabel.Text = "Foreman v2.0 - dev.13";
 			this.VersionLabel.TextAlign = System.Drawing.ContentAlignment.TopRight;
 			// 
 			// MainForm
